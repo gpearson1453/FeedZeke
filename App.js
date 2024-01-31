ba3e1f29f3ed01c613362c66adc0544b5cc37694
George Pearson <gpearson31@gatech.edu>
2024-01-30 20:56:05 -0500

    add my files

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View} from "react-native";
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require("./assets/images/turtle.jpg");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="unchecked" label="Feed Zeke" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#cee9d7',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 320,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
---
+++ components/Button.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+
+export default function Button({ label }) {
+    const [theme, setTheme] = useState("unchecked");
+    const handlePress = () => {
+        setTheme(theme === "checked" ? "unchecked" : "checked");
+        theme === "checked" ? alert('Feed Zeke!') : alert('Zeke has been fed!');
+    };
+
+    return (
+        <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#bcbcbc", borderRadius: 18 }]}>
+        <Pressable
+            style={[styles.button, { backgroundColor: "#fff" }]}
+            onPress={handlePress}
+        >
+            {theme === "unchecked" ? (
+            <FontAwesome6
+                name="bowl-food"
+                size={18}
+                color="#25292e"
+                style={styles.buttonIcon}
+            />
+            ) : (
+            <FontAwesome
+                name="check"
+                size={18}
+                color="#51bf21"
+                style={styles.buttonIcon}
+            />
+            )}
+            <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+        </Pressable>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
